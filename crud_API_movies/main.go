f618ce96a905332d0fa3f87d7713b80ec0c9ee98
aom31 <97218117+aom31@users.noreply.github.com>
2022-07-08 21:43:06 +0700

    Update main.go

--- crud_API_movies/main.go
+++ crud_API_movies/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
@@ -21,6 +24,47 @@ type Director struct {
 }
 
 var movies []Movie
+
+func getMovies(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(res).Encode(movies)
+}
+
+func getMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+	for _, item := range movies {
+		if item.Id == params["id"] {
+			json.NewEncoder(res).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(res).Encode(movies) //send all remain movies back
+}
+func createMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	var movie Movie
+	_ = json.NewDecoder(req.Body).Decode(&movie)
+	movie.Id = strconv.Itoa(rand.Intn(1000000))
+	movies = append(movies, movie)
+	json.NewEncoder(res).Encode(movie)
+
+}
+func updateMovie() {
+
+}
+func deleteMovie(res http.ResponseWriter, req *http.Request) {
+	res.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(req)
+	for index, item := range movies {
+		if item.Id == params["id"] {
+			movies = append(movies[:index], movies[index+1:]...)
+			break
+
+		}
+	}
+
+}
 
 func main() {
 	r := mux.NewRouter()
