e9abcbe4250720809d4c32c4892c608dd3e7abd7
aom31 <97218117+aom31@users.noreply.github.com>
2022-07-09 16:13:48 +0700

    Update App.go

--- manage_bookstore_API/config/App.go
+++ manage_bookstore_API/config/App.go
@@ -10,7 +10,7 @@ var (
 )
 
 func Connect() {
-	dB, err := gorm.Open("mysql", "root:3143@/managementweb?charset=utf8&parseTime = True&loc=Local")
+	dB, err := gorm.Open("mysql", "root:3143@/managementweb?charset=utf8&parseTime=True&loc=Local")
 
 	if err != nil {
 		panic(err)
